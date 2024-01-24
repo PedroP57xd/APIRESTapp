cb92a7e3632ab5948bba18f34ce20a95cb849d91
PEDRO <130096211+PedroP57xd@users.noreply.github.com>
2024-01-24 12:31:03 -0500

    commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { APIServiceService } from './../Services/apiservice.service';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,25 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  // Constructor del componente que recibe una instancia del servicio APIServiceService.
+  constructor(private api: APIServiceService) {}
 
+  // Método para obtener datos de un Pokémon dado su ID.
+  getPokemonDataID(id: number) {
+    try {
+      let pokemon;  // Declara una variable local para almacenar el nombre del Pokémon.
+
+      // Llama al método getPokemon del servicio y se suscribe al observable.
+      this.api.getPokemonID(id).subscribe((response => {
+        // Dentro de la función de éxito de la suscripción:
+        // Asigna el nombre del Pokémon a la variable local 'pokemon'.
+        pokemon = response.name;
+        // Imprime el nombre del Pokémon en la consola.
+        console.log(pokemon);
+      }));
+    } catch (error) {
+      // Captura cualquier error que pueda ocurrir durante la suscripción y lo imprime en la consola.
+      console.log(error);
+    }
+  }
 }
